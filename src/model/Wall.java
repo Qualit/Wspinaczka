22d08de8b9f6b7b37ca4b277e94fe14e5b4cb926
Mariusz Kuc <mkucpl@gmail.com>
2015-05-19 15:46:39 +0200

    second commit

--- src/model/Wall.java
+++ src/model/Wall.java
@@ -12,19 +12,19 @@ public class Wall
      }
 	
      private HashMap<Enum, Leg> legs; // konczyny pajaka
-	TreeSet<Grip> grips; // uchwyty na scianie
+     TreeSet<Grip> grips; // uchwyty na scianie
 	
 	// dla zadanej konczyny zwraca kontener dopuszczalnych 
 	// uchwytow do przejscia
 	
-	private Set getFeasibleGrips(Leg currentLeg, double radius)
-	{
-	    return null;
-	}
+     private Set getFeasibleGrips(Leg currentLeg, double radius)
+     {
+	 return null;
+     }
 	
 	
-	public Boolean move(Leg leg, Grip nextGrip)
-	{
+     public Boolean move(Leg leg, Grip nextGrip)
+     {
 		leg.addGrip(nextGrip);
 		leg.updateCost(nextGrip);
 		
