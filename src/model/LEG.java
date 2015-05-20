f2f8c0768266901a78a3ebc4629e369eccb05b87
Mariusz Kuc <mkucpl@gmail.com>
2015-05-20 14:21:16 +0200

    dodanie spidera

---
+++ src/model/LEG.java
@@ -0,0 +1,6 @@
+package model;
+
+public enum LEG
+{
+    LEFT_HAND, RIGHT_HAND, LEFT_FOOT, RIGHT_FOOT;
+}
--- src/model/Spider.java
+++ src/model/Spider.java
@@ -1,6 +1,12 @@
 package model;
 
+import java.util.List;
+import java.util.Map;
+
 public class Spider
 {
+    /* Spider's legs */
+   private Map<LEG, Leg> legs; // 
+    
 
 }
